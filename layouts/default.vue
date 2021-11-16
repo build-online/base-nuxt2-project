8253cf1e343440dfb6776aaac20fea1ec6cbc93a
Dan Bzurovski <danbz@buildonline.co>
2021-11-16 18:12:47 -0300

    Add base tailwind components

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>
+      <Sidebar />
+      <div class="md:pl-64 flex flex-col flex-1">
+        <main class="flex-1">
+          <div class="py-6">
+            <div class="mx-auto px-4 sm:px-6 md:px-8">
+              <Nuxt />
+            </div>
+          </div>
+        </main>
+        <portal-target name="main" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from '~/components/shared/layout/Sidebar'
+export default {
+  components: { Sidebar }
+}
+</script>
+
+<style scoped>
+
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#eb5757',
+        'primary-80': '#EF7979',
+        secondary: '#161616',
+        tertiary: '#161616',
+        success: '#34D399',
+        warning: '#FCD34D',
+      },
+      borderColor: {
+        primary: '#eb5757',
+        'primary-80': '#EF7979',
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      opacity: ['disabled'],
+      cursor: ['disabled'],
+    },
   },
-  plugins: [],
-}
+  plugins: [require('@tailwindcss/forms')],
+};
