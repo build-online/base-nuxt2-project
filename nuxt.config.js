41906eb116869e2073c8f7867d53622636e08975
Juan Vargas <juanmvg2006@gmail.com>
2021-11-25 15:16:54 -0300

    Add nuxt auth package with initial tokenized setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,6 @@
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nuxt-tailwind-boilerplate',
     meta: [
@@ -16,46 +14,71 @@ export default {
     ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '~/assets/css/custom.css',
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/vuePortal.js',
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/auth-next',
     '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
   ],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  auth: {
+    strategies: {
+      local: {
+        token: {
+          property: 'token',
+          global: true,
+        },
+        user: {
+          property: 'user',
+        },
+        endpoints: {
+          login: {
+            url: `${process.env.NUXT_ENV_BASE_URL}/api/v1/auth/login`,
+            method: 'post',
+          },
+          logout: {
+            url: `${process.env.NUXT_ENV_BASE_URL}/api/v1/auth/logout`,
+            method: 'post',
+          },
+          user: {
+            url: `${process.env.NUXT_ENV_BASE_URL}/api/v1/auth/user`,
+            method: 'get',
+          },
+        },
+      },
+    },
+    redirect: {
+      login: '/auth/login',
+      logout: '/',
+      callback: '/auth/login',
+      home: false,
+    },
+  },
 
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  axios: {
+    baseURL: process.env.NUXT_ENV_BASE_URL,
+  },
+
   pwa: {
     manifest: {
       lang: 'en'
     }
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
 }
